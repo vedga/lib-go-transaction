8e06e9afd2bffcdd33a56f3fb369f10add4683d1
Igor V. Nikolaev <support@vedga.com>
2026-04-22 21:41:11 +0300

    Add example for task building and data passing.

--- examples/async/main.go
+++ examples/async/main.go
@@ -8,29 +8,102 @@ import (
 )
 
 type (
+	dataA struct {
+		// ValueA started from upper case therefor it serialized as "v" JSON field
+		ValueA string `json:"v"`
+	}
+
+	// taskA is task A context
 	taskA struct {
-		CtxValueA int
+		// dataManager started from lower case letter therefore it is not serialized and must be initialized via constructor
+		dataManager *data.Manager
+		// CtxValueA started from upper case letter therefore it serialized as "ctx_value_a" JSON field
+		CtxValueA int `json:"ctx_value_a"`
+		// CtxValueB started from upper case letter therefore it serialized as "CtxValueB" JSON field
 		CtxValueB string
 	}
 
+	// taskB is task B context
 	taskB struct {
-		CtxValueA int
+		dataManager *data.Manager
+		CtxValueA   int
 	}
 )
 
 const (
-	kindA = "taskA"
-	kindB = "taskB"
+	kindDataA   = "dataA"
+	kindDataInt = "dataInt"
+	kindTaskA   = "taskA"
+	kindTaskB   = "taskB"
 )
 
 // Run is implementation of the transaction.Task interface for task A
 func (i *taskA) Run(ctx context.Context, kind string, tx transaction.Transaction) error {
+	o, e := i.dataManager.New(kindDataInt, func(o any) error {
+		p, e := data.As[*int](o)
+		if e != nil {
+			return e
+		}
+
+		// Setup int value
+		*p = 1234
+
+		return nil
+	})
+
+	// Encode modified data
+	var encodedDataB data.Bytes
+	encodedDataB, e = i.dataManager.Encode(kindDataInt, o)
+
+	// Allocate data of type dataA. Also, possible pass initially data setup if necessary.
+	o, e = i.dataManager.New(kindDataA)
+	// Check error
+	_ = e
+
+	var p *dataA
+	p, e = data.As[*dataA](o)
+	// Check error
+	_ = e
+
+	// Setup data values
+	p.ValueA = `Some value`
+
+	// Encode modified data
+	var encodedDataA data.Bytes
+	encodedDataA, e = i.dataManager.Encode(kindDataA, o)
+	// Check error
+
+	// Push to the data stack
+	tx.PushData(encodedDataA, encodedDataB)
+
+	return nil
+}
+
+// Run is implementation of the transaction.Task interface for task B
+func (i *taskB) Run(ctx context.Context, kind string, tx transaction.Transaction) error {
+	encodedData, present := tx.PopData()
+	if present {
+		// Must be encodedDataB as used stack order
+		dataKind, o, e := i.dataManager.Decode(encodedData)
+		// Check error
+		_ = e
+		// Use for type check if required
+		_ = dataKind
+
+		var d *dataA
+		d, e = data.As[*dataA](o)
+		// Check error
+		_ = e
+		// Use data
+		_ = d
+	}
+
 	return nil
 }
 
 // OperationTaskA return task A with custom initially context
 func OperationTaskA(txManager *transaction.Manager, valueA int, valueB string) (transaction.Task, error) {
-	return txManager.NewTask(kindA, func(o any) error {
+	return txManager.NewTask(kindTaskA, func(o any) error {
 		p, e := data.As[*taskA](o)
 		if e != nil {
 			return e
@@ -45,7 +118,7 @@ func OperationTaskA(txManager *transaction.Manager, valueA int, valueB string) (
 
 // OperationTaskB return task B with custom initially context
 func OperationTaskB(txManager *transaction.Manager, valueA int) (transaction.Task, error) {
-	return txManager.NewTask(kindB, func(o any) error {
+	return txManager.NewTask(kindTaskB, func(o any) error {
 		p, e := data.As[*taskB](o)
 		if e != nil {
 			return e
@@ -57,21 +130,24 @@ func OperationTaskB(txManager *transaction.Manager, valueA int) (transaction.Tas
 	})
 }
 
-// Run is implementation of the transaction.Task interface for task B
-func (i *taskB) Run(ctx context.Context, kind string, tx transaction.Transaction) error {
-	return nil
-}
-
 func main() {
+	// Create data manager and register supported data types
+	dataManager := data.NewManager(
+		data.WithProducer(kindDataA, data.NewProducer[dataA]()),
+		data.WithProducer(kindDataInt, data.NewProducer[int]()),
+	)
+
 	// Create transaction manager
 	txManager := transaction.NewManager(
-		transaction.WithTxTaskProducer(kindA, func(setup ...data.Setup) (transaction.Task, error) {
+		transaction.WithTxTaskProducer(kindTaskA, func(setup ...data.Setup) (transaction.Task, error) {
 			producer := data.NewProducer[taskA]()
 
 			task, e := producer(append([]data.Setup{
 				// taskA registration
 				data.NewSetup[taskA](func(o *taskA) error {
 					// Special constructor for task A if required
+					o.dataManager = dataManager
+
 					return nil
 				}),
 			}, setup...)...)
@@ -81,13 +157,15 @@ func main() {
 
 			return data.As[transaction.Task](task)
 		}),
-		transaction.WithTxTaskProducer(kindB, func(setup ...data.Setup) (transaction.Task, error) {
+		transaction.WithTxTaskProducer(kindTaskB, func(setup ...data.Setup) (transaction.Task, error) {
 			producer := data.NewProducer[taskB]()
 
 			task, e := producer(append([]data.Setup{
 				// taskB registration
 				data.NewSetup[taskB](func(o *taskB) error {
 					// Special constructor for task B if required
+					o.dataManager = dataManager
+
 					return nil
 				}),
 			}, setup...)...)
@@ -108,7 +186,7 @@ func main() {
 	_ = e
 
 	// Direct add task A to the transaction
-	e = tx.QueueTask(kindA, task)
+	e = tx.QueueTask(kindTaskA, task)
 	// Check error
 	_ = e
 
@@ -118,7 +196,7 @@ func main() {
 	_ = e
 
 	var encodedTask data.Bytes
-	encodedTask, e = txManager.EncodeTask(kindB, task)
+	encodedTask, e = txManager.EncodeTask(kindTaskB, task)
 	// Check error
 	_ = e
 
